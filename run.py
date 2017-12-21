d012c11a151561ab818c013db88bb8f875965b95
miomjom <kratosjon@gmail.com>
2017-12-21 12:52:42 +0100

    add features variable

--- run.py
+++ run.py
@@ -35,6 +35,7 @@ if __name__=="__main__":
     ####### Decision boolean #######
     trained=False
     logistic=False
+    feature=False
 
     ####### Use of pretrained model ########
     if(len(sys.argv)>1):
